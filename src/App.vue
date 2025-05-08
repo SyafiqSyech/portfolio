7f4d83248d286e9f5975544a5c080633966d3ad9
Syaf <syafiq.syech@gmail.com>
2025-05-08 19:30:28 +0700

    hardcode, testing design

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,146 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-  <div class="text-3xl">Hello World</div>
+  <div class="flex flex-col items-center pb-2">
+    <div class="flex items-center justify-center group relative mb-4 hover:mb-8 transition-all duration-300">
+      <div class="w-36 h-48 rounded-[100%_100%_0.25rem_0.25rem] bg-cover saturate-0 contrast-125 left-0 over:contrast-100 group-hover:saturate-100 group-hover:scale-105 transition-all duration-300" style="background-image: url('/src/assets/profile.jpg');"></div>
+      <div class="absolute text-xl bottom-16 group-hover:scale-150 group-hover:bottom-18 -left-12 group-hover:-left-16 transition-all duration-300 pointer-events-none">🙠</div>
+      <div class="absolute text-xl bottom-16 group-hover:scale-150 group-hover:bottom-18 -right-12 group-hover:-right-16 transition-all duration-300 pointer-events-none">🙢</div>
+      <div class="absolute text-3xl -top-24 blur-sm opacity-0 group-hover:-top-14 group-hover:blur-none group-hover:opacity-100 transition-all duration-300 pointer-events-none">🜲</div>
+    </div>
+    <div class="!font-semibold">ABDULLAH SYAFIQ</div>
+    <div class="text-xl !font-semibold">SOFTWARE DEVELOPER</div>
+  </div>
+  Based in
+  <div class="italic">🞠 South Jakarta, Indonesia</div>
+
+  <div class="py-24 text-center">🞄&emsp;🞟&emsp;🟄&emsp;🟅&emsp;🟆&emsp;🟅&emsp;🟄&emsp;🞟&emsp;🞄</div>
+
+  <div class="flex flex-col">
+    <div></div>
+    <div class="text-heading pb-8">🙦 EXPERIENCE</div>
+    <div class="">
+      <div>
+        <div class="py-4 flex flex-col justify-center relative hover:px-3 transition-all duration-300 group">
+          <div class="absolute h-6 -left-12 blur-sm opacity-0 transition-all duration-300 group-hover:opacity-100 group-hover:-left-6 group-hover:blur-none pointer-events-none">🙣</div>
+          <div class="flex justify-between">
+            <div class="">P.T Periksa Solusi Indonesia</div>
+            <div class="text-end">Feb 2024 - Feb&nbsp;2025</div>
+          </div>
+          <div class="flex justify-between italic">
+            <div class="">Frontend Developer Intern</div>
+            <div class="text-end">Central Jakarta, Indonesia (Remote)</div>
+          </div>
+          <div class="absolute h-6 -right-12 blur-sm opacity-0 transition-all duration-300 group-hover:opacity-100 group-hover:-right-6 group-hover:blur-none pointer-events-none">🙡</div>
+        </div>
+        <!-- <div class="mx-16 pt-2">
+          <div class="w-full">
+            <ul style="list-style-type: '🞟  ';">
+              <li>Maintained an <span class="bg-[#fafafa3a]">SaaS dashboard application</span> by developing and updating interactive tables with advanced filtering, <span class="bg-[#fafafa3a]">CRUD operations</span>, and complex forms using <span class="bg-[#fafafa3a]">Angular</span>.</li>
+              <li>Built and updated <span class="bg-[#fafafa3a]">RESTful API</span> services and data models with <span class="bg-[#fafafa3a]">TypeScript</span>.</li>
+              <li>Contributed to a separate application by building printable form templates using <span class="bg-[#fafafa3a]">PHP</span> and <span class="bg-[#fafafa3a]">HTML</span>, and utilized <span class="bg-[#fafafa3a]">DBeaver</span> for data validation.</li>
+            </ul>
+          </div>
+        </div> -->
+      </div>
+      <div>
+        <div class="py-4 flex flex-col justify-center relative hover:px-3 transition-all duration-300 group">
+          <div class="absolute h-6 -left-12 blur-sm opacity-0 transition-all duration-300 group-hover:opacity-100 group-hover:-left-6 group-hover:blur-none pointer-events-none">🙣</div>
+          <div class="flex justify-between">
+            <div class="">Your Company Hopefully</div>
+            <div class="text-end">Presently</div>
+          </div>
+          <div class="italic">Software Engineer</div>
+          <div class="absolute h-6 -right-12 blur-sm opacity-0 transition-all duration-300 group-hover:opacity-100 group-hover:-right-6 group-hover:blur-none pointer-events-none">🙡</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-24 text-center">🞄&emsp;🞟&emsp;🟄&emsp;🟅&emsp;🟆&emsp;🟅&emsp;🟄&emsp;🞟&emsp;🞄</div>
+
+  <div class="flex flex-col">
+    <div></div>
+    <div class="text-heading pb-8">🙦 PROJECTS</div>
+    <div class="">
+      <div class="py-4 flex items-center relative group">
+        <div class="absolute h-6 -left-12 blur-sm opacity-0 transition-all duration-300 group-hover:opacity-100 group-hover:-left-6 group-hover:blur-none pointer-events-none">🙣</div>
+        <div class="w-full flex flex-col sm:flex-row-reverse gap-6 justify-between">
+          <div class="sm:w-1/2 group-hover:pl-3 transition-all duration-300">
+            <div class="w-full h-48 rounded-sm overflow-hidden">
+              <div class="w-full h-full bg-cover sm:saturate-0 sm:contrast-125 sm:group-hover:contrast-100 sm:group-hover:saturate-100 group-hover:scale-105 transition-all duration-300" style="background-image: url('/src/assets/test.png');"></div>
+            </div>
+          </div>
+          <div class="sm:w-1/2 relative left-0 group-hover:left-3 transition-all duration-300">
+            <div>Mod Manager (In Progress)</div>
+            <div class="italic pb-2 sm:pb-4">Fullstack Developer</div>
+            <div class="text-sm">
+              React
+              🞟 Typescript
+              🞟 TailwindCSS
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="py-4 flex items-center relative group">
+        <div class="absolute h-6 -left-12 blur-sm opacity-0 transition-all duration-300 group-hover:opacity-100 group-hover:-left-6 group-hover:blur-none pointer-events-none">🙣</div>
+        <div class="w-full flex flex-col sm:flex-row-reverse gap-6 justify-between">
+          <div class="sm:w-1/2 group-hover:pl-3 transition-all duration-300">
+            <div class="w-full h-48 rounded-sm overflow-hidden">
+              <div class="w-full h-full bg-cover sm:saturate-0 sm:contrast-125 sm:group-hover:contrast-100 sm:group-hover:saturate-100 group-hover:scale-105 transition-all duration-300" style="background-image: url('/src/assets/test2.png');"></div>
+            </div>
+          </div>
+          <div class="sm:w-1/2 relative left-0 group-hover:left-3 transition-all duration-300">
+            <div>Layzy</div>
+            <div class="italic pb-2 sm:pb-4">Project Leader / Frontend Developer</div>
+            <div class="text-sm">
+              React
+              🞟 React Libraries
+              🞟 Typescript
+              🞟 TailwindCSS
+              🞟 Vite
+              🞟 Netlify
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="py-24 text-center">🞄&emsp;🞟&emsp;🟄&emsp;🟅&emsp;🟆&emsp;🟅&emsp;🟄&emsp;🞟&emsp;🞄</div>
+
+  <div class="flex flex-col">
+    <div></div>
+    <div class="text-heading pb-8">🙦 SKILLS</div>
+    <div class="grid grid-cols-2 sm:grid-cols-3 gap-4 py-2">
+      <div>🟄 React</div>
+      <div>🟄 TailwindCSS</div>
+      <div>🟄 Vue</div>
+      <div>🟄 Angular</div>
+      <div>🟄 Typescript</div>
+      <div>🟄 Rest API</div>
+      <div>🟄 Git/Github</div>
+      <div>🟄 SQL</div>
+      <div>🟄 HTML/CSS</div>
+    </div>
+  </div>
+  
+  <div class="py-24 text-center">🞄&emsp;🞟&emsp;🟄&emsp;🟅&emsp;🟆&emsp;🟅&emsp;🟄&emsp;🞟&emsp;🞄</div>
+
+  <div class="flex flex-col">
+    <div></div>
+    <div class="text-heading pb-8">🙦 EDUCATION</div>
+    <div class="">
+      <div class="py-2">
+        <div class="flex justify-between">
+          <div class="">Bina Nusantara University</div>
+          <div class="text-end">2025</div>
+        </div>
+        <div class="italic">Bachelor of Computer Science / 3.87 GPA / 3.5 Years</div>
+      </div>
+    </div>
+  </div>
+
 </template>
 
-<style scoped>
-
-</style>
+<style></style>
