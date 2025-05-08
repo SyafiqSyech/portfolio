5c8825ea9fac43a327918f034cb7bf8ea3c8c77e
Syaf <syafiq.syech@gmail.com>
2025-05-08 20:50:31 +0700

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
 </script>
 
 <template>
-  <div class="flex flex-col items-center pb-2">
+  <div class="flex flex-col items-center pb-8">
     <div class="flex items-center justify-center group relative mb-4 hover:mb-8 transition-all duration-300">
       <div class="w-36 h-48 rounded-[100%_100%_0.25rem_0.25rem] bg-cover saturate-0 contrast-125 left-0 over:contrast-100 group-hover:saturate-100 group-hover:scale-105 transition-all duration-300" style="background-image: url('/src/assets/profile.jpg');"></div>
-      <div class="absolute text-xl bottom-16 group-hover:scale-150 group-hover:bottom-18 -left-12 group-hover:-left-16 transition-all duration-300 pointer-events-none">🙠</div>
-      <div class="absolute text-xl bottom-16 group-hover:scale-150 group-hover:bottom-18 -right-12 group-hover:-right-16 transition-all duration-300 pointer-events-none">🙢</div>
+      <div class="absolute text-xl bottom-16 group-hover:scale-150 group-hover:bottom-18 -left-10 group-hover:-left-16 transition-all duration-300 pointer-events-none">🙠</div>
+      <div class="absolute text-xl bottom-16 group-hover:scale-150 group-hover:bottom-18 -right-10 group-hover:-right-16 transition-all duration-300 pointer-events-none">🙢</div>
       <div class="absolute text-3xl -top-24 blur-sm opacity-0 group-hover:-top-14 group-hover:blur-none group-hover:opacity-100 transition-all duration-300 pointer-events-none">🜲</div>
     </div>
     <div class="!font-semibold">ABDULLAH SYAFIQ</div>
@@ -15,23 +15,24 @@
   Based in
   <div class="italic">🞠 South Jakarta, Indonesia</div>
 
-  <div class="py-24 text-center">🞄&emsp;🞟&emsp;🟄&emsp;🟅&emsp;🟆&emsp;🟅&emsp;🟄&emsp;🞟&emsp;🞄</div>
+  <div class="py-24 text-center">🟆 🟅 🟄 🞟 🞄 🞟 🟄 🟅 🟆</div>
+  <!-- <div class="py-24 text-center">🞄&emsp;🞟&emsp;🟄&emsp;🟅&emsp;🟆&emsp;🟅&emsp;🟄&emsp;🞟&emsp;🞄</div> -->
 
   <div class="flex flex-col">
     <div></div>
-    <div class="text-heading pb-8">🙦 EXPERIENCE</div>
-    <div class="">
+    <div class="text-heading pb-8">🙦&ensp;EXPERIENCE</div>
+    <div class="flex flex-col gap-4">
       <div>
-        <div class="py-4 flex flex-col justify-center relative hover:px-3 transition-all duration-300 group">
-          <div class="absolute h-6 -left-12 blur-sm opacity-0 transition-all duration-300 group-hover:opacity-100 group-hover:-left-6 group-hover:blur-none pointer-events-none">🙣</div>
-          <div class="flex justify-between">
+        <div class="flex flex-col justify-center relative hover:px-3 transition-all duration-300 group">
+          <div class="grid sm:grid-cols-2 gap-x-4">
             <div class="">P.T Periksa Solusi Indonesia</div>
-            <div class="text-end">Feb 2024 - Feb&nbsp;2025</div>
+            <div class="text-sm sm:text-base sm:text-end">Feb 2024 - Feb&nbsp;2025<span class="sm:hidden"> / Central Jakarta, Indonesia (Remote)</span></div>
+            <div class="italic">Frontend Developer Intern</div>
+            <div class="italic text-end hidden sm:block">Central Jakarta, Indonesia (Remote)</div>
           </div>
           <div class="flex justify-between italic">
-            <div class="">Frontend Developer Intern</div>
-            <div class="text-end">Central Jakarta, Indonesia (Remote)</div>
           </div>
+          <div class="absolute h-6 -left-12 blur-sm opacity-0 transition-all duration-300 group-hover:opacity-100 group-hover:-left-6 group-hover:blur-none pointer-events-none">🙣</div>
           <div class="absolute h-6 -right-12 blur-sm opacity-0 transition-all duration-300 group-hover:opacity-100 group-hover:-right-6 group-hover:blur-none pointer-events-none">🙡</div>
         </div>
         <!-- <div class="mx-16 pt-2">
@@ -45,29 +46,30 @@
         </div> -->
       </div>
       <div>
-        <div class="py-4 flex flex-col justify-center relative hover:px-3 transition-all duration-300 group">
-          <div class="absolute h-6 -left-12 blur-sm opacity-0 transition-all duration-300 group-hover:opacity-100 group-hover:-left-6 group-hover:blur-none pointer-events-none">🙣</div>
-          <div class="flex justify-between">
-            <div class="">Your Company Hopefully</div>
-            <div class="text-end">Presently</div>
+        <div class="flex flex-col justify-center relative hover:px-3 transition-all duration-300 group">
+          <div class="grid sm:grid-cols-2 gap-x-4">
+            <div class="">Your Company Hopefully :)</div>
+            <div class="text-sm sm:text-base sm:text-end">Tomorrow</div>
+            <div class="italic">Software Engineer</div>
           </div>
-          <div class="italic">Software Engineer</div>
+          <div class="absolute h-6 -left-12 blur-sm opacity-0 transition-all duration-300 group-hover:opacity-100 group-hover:-left-6 group-hover:blur-none pointer-events-none">🙣</div>
           <div class="absolute h-6 -right-12 blur-sm opacity-0 transition-all duration-300 group-hover:opacity-100 group-hover:-right-6 group-hover:blur-none pointer-events-none">🙡</div>
         </div>
       </div>
     </div>
   </div>
-  <div class="py-24 text-center">🞄&emsp;🞟&emsp;🟄&emsp;🟅&emsp;🟆&emsp;🟅&emsp;🟄&emsp;🞟&emsp;🞄</div>
+  <div class="py-24 text-center">🟆 🟅 🟄 🞟 🞄 🞟 🟄 🟅 🟆</div>
+  <!-- <div class="py-24 text-center">🞄&emsp;🞟&emsp;🟄&emsp;🟅&emsp;🟆&emsp;🟅&emsp;🟄&emsp;🞟&emsp;🞄</div> -->
 
   <div class="flex flex-col">
     <div></div>
-    <div class="text-heading pb-8">🙦 PROJECTS</div>
-    <div class="">
-      <div class="py-4 flex items-center relative group">
+    <div class="text-heading pb-8">🙦&ensp;PROJECTS</div>
+    <div class="flex flex-col gap-4">
+      <div class="flex items-center relative group">
         <div class="absolute h-6 -left-12 blur-sm opacity-0 transition-all duration-300 group-hover:opacity-100 group-hover:-left-6 group-hover:blur-none pointer-events-none">🙣</div>
-        <div class="w-full flex flex-col sm:flex-row-reverse gap-6 justify-between">
+        <div class="w-full flex flex-col sm:flex-row-reverse gap-4 justify-between">
           <div class="sm:w-1/2 group-hover:pl-3 transition-all duration-300">
-            <div class="w-full h-48 rounded-sm overflow-hidden">
+            <div class="w-full aspect-video sm:h-48 rounded-sm overflow-hidden">
               <div class="w-full h-full bg-cover sm:saturate-0 sm:contrast-125 sm:group-hover:contrast-100 sm:group-hover:saturate-100 group-hover:scale-105 transition-all duration-300" style="background-image: url('/src/assets/test.png');"></div>
             </div>
           </div>
@@ -82,11 +84,11 @@
           </div>
         </div>
       </div>
-      <div class="py-4 flex items-center relative group">
+      <div class="flex items-center relative group">
         <div class="absolute h-6 -left-12 blur-sm opacity-0 transition-all duration-300 group-hover:opacity-100 group-hover:-left-6 group-hover:blur-none pointer-events-none">🙣</div>
-        <div class="w-full flex flex-col sm:flex-row-reverse gap-6 justify-between">
+        <div class="w-full flex flex-col sm:flex-row-reverse gap-4 justify-between">
           <div class="sm:w-1/2 group-hover:pl-3 transition-all duration-300">
-            <div class="w-full h-48 rounded-sm overflow-hidden">
+            <div class="w-full aspect-video sm:h-48 rounded-sm overflow-hidden">
               <div class="w-full h-full bg-cover sm:saturate-0 sm:contrast-125 sm:group-hover:contrast-100 sm:group-hover:saturate-100 group-hover:scale-105 transition-all duration-300" style="background-image: url('/src/assets/test2.png');"></div>
             </div>
           </div>
@@ -107,11 +109,12 @@
     </div>
   </div>
 
-  <div class="py-24 text-center">🞄&emsp;🞟&emsp;🟄&emsp;🟅&emsp;🟆&emsp;🟅&emsp;🟄&emsp;🞟&emsp;🞄</div>
+  <div class="py-24 text-center">🟆 🟅 🟄 🞟 🞄 🞟 🟄 🟅 🟆</div>
+  <!-- <div class="py-24 text-center">🞄&emsp;🞟&emsp;🟄&emsp;🟅&emsp;🟆&emsp;🟅&emsp;🟄&emsp;🞟&emsp;🞄</div> -->
 
   <div class="flex flex-col">
     <div></div>
-    <div class="text-heading pb-8">🙦 SKILLS</div>
+    <div class="text-heading pb-8">🙦&ensp;SKILLS</div>
     <div class="grid grid-cols-2 sm:grid-cols-3 gap-4 py-2">
       <div>🟄 React</div>
       <div>🟄 TailwindCSS</div>
@@ -125,11 +128,12 @@
     </div>
   </div>
   
-  <div class="py-24 text-center">🞄&emsp;🞟&emsp;🟄&emsp;🟅&emsp;🟆&emsp;🟅&emsp;🟄&emsp;🞟&emsp;🞄</div>
+  <div class="py-24 text-center">🟆 🟅 🟄 🞟 🞄 🞟 🟄 🟅 🟆</div>
+  <!-- <div class="py-24 text-center">🞄&emsp;🞟&emsp;🟄&emsp;🟅&emsp;🟆&emsp;🟅&emsp;🟄&emsp;🞟&emsp;🞄</div> -->
 
   <div class="flex flex-col">
     <div></div>
-    <div class="text-heading pb-8">🙦 EDUCATION</div>
+    <div class="text-heading pb-8">🙦&ensp;EDUCATION</div>
     <div class="">
       <div class="py-2">
         <div class="flex justify-between">
