cb612c1d11ca7b6c12250c669db52949af80823b
Syaf <syafiq.syech@gmail.com>
2025-05-13 18:30:53 +0700

    added design system & section layout

---
+++ src/components/layout/Section.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="grid lg:grid-cols-[0_1fr]">
+
+    <div class="font-semibold text-lg pb-16 relative">
+      <div class="absolute lg:right-44 xl:right-64 w-full">
+        Experience
+      </div>
+    </div>
+    
+    <slot></slot>
+  
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineProps({
+    title: {
+      type: String,
+      required: true
+    },
+  });
+</script>
