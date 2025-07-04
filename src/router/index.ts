cc5a3753d0efbfde58bf2b56a81b42446c6c6fb4
Syaf <syafiq.syech@gmail.com>
2025-07-04 20:51:29 +0700

    updates

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,47 +1,77 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
-import ProjectsPage from '../views/ProjectsPage.vue'
-import ProjectDetailsPage from '../views/ProjectDetailsPage.vue'
-import ExperiencesPage from '../views/ExperiencesPage.vue'
-import ExperienceDetailsPage from '../views/ExperienceDetailsPage.vue'
 import NotFoundPage from '../views/NotFoundPage.vue'
+
+const ProjectsPage = () => import('../views/ProjectsPage.vue')
+const ProjectDetailsPage = () => import('../views/ProjectDetailsPage.vue')
+const ExperiencesPage = () => import('../views/ExperiencesPage.vue')
+const ExperienceDetailsPage = () => import('../views/ExperienceDetailsPage.vue')
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    title?: string;
+  }
+}
 
 const routes = [
 	{
 		path: '/',
 		name: 'home',
-		component: HomePage
+		component: HomePage,
+		meta: { 
+			title: 'Abdullah Syafiq'
+		}
 	},
 	{
 		path: '/projects',
 		name: 'projects',
-		component: ProjectsPage
+		component: ProjectsPage,
+		meta: { 
+			title: 'Projects | Abdullah Syafiq'
+		}
 	},
 	{
 		path: '/projects/:projectId',
 		name: 'project-details',
-		component: ProjectDetailsPage
+		component: ProjectDetailsPage,
+		meta: { 
+			title: 'Project Details | Abdullah Syafiq'
+		}
 	},
 	{
 		path: '/experiences',
 		name: 'experiences',
-		component: ExperiencesPage
+		component: ExperiencesPage,
+		meta: { 
+			title: 'Experiences | Abdullah Syafiq'
+		}
 	},
 	{
 		path: '/experiences/:experienceId',
 		name: 'experience-details',
-		component: ExperienceDetailsPage
+		component: ExperienceDetailsPage,
+		meta: { 
+			title: 'Experience Details | Abdullah Syafiq'
+		}
 	},
 	{
 		path: '/:pathMatch(.*)*',
 		name: 'not-found',
-		component: NotFoundPage
+		component: NotFoundPage,
+		meta: { 
+			title: 'Page Not Found | Abdullah Syafiq'
+		}
 	}
 ]
 
 const router = createRouter({
 	history: createWebHistory(),
 	routes
+})
+
+router.beforeEach((to, _from, next) => {
+	document.title = to.meta.title as string || 'Abdullah Syafiq'
+	next()
 })
 
 export default router
